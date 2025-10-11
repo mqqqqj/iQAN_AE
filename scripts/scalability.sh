e9dd546be51440f28dd9023581139e1cc5b4b830
mqqqqj <moqiji@outlook.com>
2025-10-11 13:34:54 +0800

    bash

---
+++ scripts/scalability.sh
@@ -0,0 +1,22 @@
+thread_num=2
+
+# L_list=(300 1100)
+
+# L_str=$(IFS=,; echo "${L_list[*]}")
+
+# taskset -c 0-$((thread_num - 1)) PSS_v5_distance_threshold_profiling \
+# /SSD/Glove200/base.1m.fbin \
+# /SSD/Glove200/query.fbin \
+# /SSD/models/nsg/glove200.L2000.R64.C200.nsg \
+# 100 output.ivecs /SSD/Glove200/gt.query.top100.bin $thread_num "$L_str" | tee -a /home/mqj/proj/ANNSLib/experiments/glove_4t_iqan.csv
+
+
+L_list=(160 170 180 190)
+
+L_str=$(IFS=,; echo "${L_list[*]}")
+
+taskset -c 0-$((thread_num - 1)) PSS_v5_distance_threshold_profiling \
+/SSD/MainSearch/base.fbin \
+/SSD/MainSearch/query_test_unique.fbin \
+/SSD/models/nsg/mainsearch.L2000.R64.C2000.nsg \
+100 output.ivecs /SSD/MainSearch/gt.test_unique.bin $thread_num "$L_str" | tee -a /home/mqj/proj/ANNSLib/experiments/mainsearch_2t_iqan.csv
