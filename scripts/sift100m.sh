ac8b305570a32a8c126f03fe9b044db9145dd119
mqqqqj <moqiji@outlook.com>
2026-01-10 17:20:33 +0800

    a

--- scripts/sift100m.sh
+++ scripts/sift100m.sh
@@ -9,5 +9,5 @@ taskset -c 0-$((thread_num - 1)) PSS_v5_distance_threshold_profiling \
 /SSD/SIFT1B/base.100M.u8bin \
 /SSD/SIFT1B/query.public.10K.u8bin \
 /SSD/models/nsg/sift100m.L200.R64.C200.nsg \
-100 output.ivecs /SSD/SIFT1B/gt.public.10K_100M_top100.bin $thread_num "$L_str" | tee -a /home/mqj/proj/ANNSLib/experiments/revision/sift100m_8t_iqan_tuned.csv
+100 output.ivecs /SSD/SIFT1B/gt.public.10K_100M_top100.bin $thread_num "$L_str" | tee -a /home/mqj/proj/ANNSLib/experiments/revision/sift100m_8t_iqan_nosync.csv
 
