96cee83b507d8b4cf39666f61149b96f0fa69b02
mqqqqj <moqiji@outlook.com>
2026-01-11 20:08:58 +0800

    a

--- scripts/gist1m.sh
+++ scripts/gist1m.sh
@@ -1,5 +1,4 @@
-# L_list=(100 100 100 100 100 100 125 150 175 200 225 250 275 300 325 350 375 400 425 450 475 500 525 550 575 600)
-L_list=(650 700 750 800 850 900 950 1000)
+L_list=(100 150 200 250 300 350 400 450 500 550 600 650 700 750 800)
 
 thread_num=8
 
@@ -9,4 +8,4 @@ taskset -c 0-$((thread_num - 1)) PSS_v5_distance_threshold_profiling \
 /SSD/GIST1M/base.fbin \
 /SSD/GIST1M/query.fbin \
 /SSD/models/nsg/gist1m.L2000.R64.C200.nsg \
-100 output.ivecs /SSD/GIST1M/gt.query.l2.top100.bin $thread_num "$L_str" | tee -a /home/mqj/proj/iQAN_AE/results/gist1m/gist1m_8t_iqan_1stepsync.csv
+100 output.ivecs /SSD/GIST1M/gt.query.l2.top100.bin $thread_num "$L_str" | tee -a /home/mqj/proj/ANNSLib/experiments/revision/gist1m_8t_iqan_nosync.csv
