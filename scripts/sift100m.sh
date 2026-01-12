14914ebf77d64914f1cf8a112c949b3c4da7ed47
mqqqqj <moqiji@outlook.com>
2026-01-12 22:25:41 +0800

    a

--- scripts/sift100m.sh
+++ scripts/sift100m.sh
@@ -1,6 +1,8 @@
 # L_list=(10 20 30 40 50 60 70 80 90 100 150 200 250 300 350 400 450 500 550 600 650 700)
 # L_list=(100 150 200 250 300 350 400 450 500 550 600 650 700 750 800 850 900 950 1000 1050 1100 1150 1200 1300 1400 1500 1600 1700 1800 1900 2000)
-L_list=(1050)
+
+L_list=(1000 1050 1100 1200)
+
 thread_num=8
 
 L_str=$(IFS=,; echo "${L_list[*]}")
@@ -9,4 +11,4 @@ taskset -c 0-$((thread_num - 1)) /home/mqj/proj/iQAN_AE/cmake-build-release/PSS_
 /SSD/SIFT1B/base.100M.u8bin \
 /SSD/SIFT1B/query.public.10K.u8bin \
 /SSD/models/nsg/sift100m.L200.R64.C200.nsg \
-100 output.ivecs /SSD/SIFT1B/gt.public.10K_100M_top100.bin $thread_num "$L_str" | tee -a /home/mqj/proj/ANNSLib/experiments/revision/sift100m_8t_iqan_tuned.csv
+100 output.ivecs /SSD/SIFT1B/gt.public.10K_100M_top100.bin $thread_num "$L_str" | tee -a /home/mqj/proj/ANNSLib/experiments/revision/sift100m_8t_iqan_nosync.csv
