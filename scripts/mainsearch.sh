9eac168655103ca9c6eb898982c82ad387e823d1
mqqqqj <moqiji@outlook.com>
2026-01-12 19:26:02 +0800

    a

--- scripts/mainsearch.sh
+++ scripts/mainsearch.sh
@@ -1,4 +1,4 @@
-L_list=(10 20 30 40 50 60 70 80 90 100 150 200 250 300 350 400 450 500 550 600 650 700 750 800 900 1000 1100)
+L_list=(100 150 200 250 300 350 400 450 500 550 600 650 700 750 800 900 1000 1100)
 thread_num=8
 
 L_str=$(IFS=,; echo "${L_list[*]}")
@@ -7,4 +7,4 @@ taskset -c 0-$((thread_num - 1)) PSS_v5_distance_threshold_profiling \
 /SSD/MainSearch/base.fbin \
 /SSD/MainSearch/query_test_unique.fbin \
 /SSD/models/nsg/mainsearch.L2000.R64.C2000.nsg \
-100 output.ivecs /SSD/MainSearch/gt.test_unique_top500.bin $thread_num "$L_str" | tee -a /home/mqj/proj/ANNSLib/experiments/revision/mainsearch_8t_1stepsync.csv
+100 output.ivecs /SSD/MainSearch/gt.test_unique_top500.bin $thread_num "$L_str" | tee -a /home/mqj/proj/ANNSLib/experiments/revision/mainsearch_8t_iqan_nosync.csv
--- scripts/t2i.sh
+++ scripts/t2i.sh
@@ -1,7 +1,7 @@
 # L_list=(10 20 30 40 50 60 70 80 90 100 125 150 175 200 225 250 275 300 325 350 375 400 425 450 475 500 525 550 575 600)
 # thread_num=16
 
-L_list=(10 20 30 40 50 60 70 80 90 100 125 150 175 200 225 250 275 300 325 350 375 400 425 450 475 500 525 550 575 600)
+L_list=(100 125 150 175 200 225 250 275 300 325 350 375 400 425 450 475 500 525)
 thread_num=8
 
 L_str=$(IFS=,; echo "${L_list[*]}")
@@ -10,4 +10,4 @@ taskset -c 0-$((thread_num - 1)) PSS_v5_distance_threshold_profiling \
 /SSD/Text-to-Image/base.10M.fbin \
 /SSD/Text-to-Image/query.10k.fbin \
 /SSD/models/nsg/t2i10m.L2000.R64.C2000.nsg \
-100 output.ivecs /SSD/Text-to-Image/gt.10K_10M.bin $thread_num "$L_str" | tee -a /home/mqj/proj/ANNSLib/experiments/revision/t2i_8t_1stepsync.csv
+100 output.ivecs /SSD/Text-to-Image/gt.10K_10M.bin $thread_num "$L_str" | tee -a /home/mqj/proj/ANNSLib/experiments/revision/t2i_8t_iqan_nosync.csv
